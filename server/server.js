71b27bbd87fc9784ffaed0433d560877ca3478ce
PaoloFiol <paolo.fiol97@gmail.com>
2024-08-07 12:12:29 -0400

    Added timeout for DB connection

--- server/server.js
+++ server/server.js
@@ -25,9 +25,10 @@ app.use(cors());
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000,
 })
 .then(() => console.log('MongoDB connected'))
-.catch(err => console.error(err));
+.catch(err => console.error('MongoDB connection error:', err));
 
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
